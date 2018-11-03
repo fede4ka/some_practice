7d91ba4693c0cb6911c803f1ddfec0e80a693e72
fede4ka <40442383+fede4ka@users.noreply.github.com>
2018-11-04 02:17:59 +0300

    Update map.scala

--- src/main/scala/map.scala
+++ src/main/scala/map.scala
@@ -4,20 +4,15 @@ object map {
     for {x <- seq}
       yield f(x)
 
-
-
   def map_2[A,B](l: List[A])(f: A => B): List[B] = {
     val buf = new collection.mutable.ListBuffer[B]
-
     def go(l: List[A]): Unit = l match {
       case Nil => ()
       case h :: t => buf += f(h); go(t)
     }
-
     go(l)
     List(buf.toList: _*)
   }
-
 
   def map_3 [A,B](l: List[A])(f: A => B): List[B] =
     l.foldRight( Nil:List[B])((h,t) =>f(h)::t)
@@ -25,15 +20,9 @@ object map {
   def map_4[A,B](l: List[A])(f: A => B): List[B] =
     l.foldLeft( Nil:List[B])((h,t) =>f(t)::h).reverse
 
-  
-
   def map_5(l: List[Int], f: Int => Int): List[Int] = {
     val buf = new collection.mutable.ListBuffer[Int]
     l foreach (buf += f(_))
     buf.toList
   }
-
-
-
-
 }
