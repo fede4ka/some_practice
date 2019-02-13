5cafc93aafa1042a1e71b670400bcaf0b0716029
fede4ka <40442383+fede4ka@users.noreply.github.com>
2019-02-13 22:03:50 +0300

    Update maximum_subarray.scala

--- src/main/scala/maximum_subarray.scala
+++ src/main/scala/maximum_subarray.scala
@@ -1,15 +1,15 @@
 object maximum_subarray extends  App {
-  val arr = Array(1,2,3,-6,5,15,-30, 20,-1,15)
+  val arr = Array(1,5,-7,2,3,15,-6,5,150,-30, 20,-144,15,11,-200)
 
-  def maxArraySublist (xs: Array[Int]): Int = {
-    var maxsum = 0
-    var sum = 0
+  def maxArraySublist (xs: Array[Int]): List[Int] = {
+    var maxsum,sum,start,end,curstart = 0
+
     for (i<- xs.indices) {
       sum+= xs(i)
-      if (sum>maxsum) maxsum = sum
-      if (sum<0) sum = 0
+      if (sum>maxsum) {maxsum = sum; end = i;start=curstart}
+      if (sum<0) {sum = 0;curstart=i+1}
     }
-    maxsum
+    List(maxsum,start,end)
   }
   print(maxArraySublist(arr))
 }
