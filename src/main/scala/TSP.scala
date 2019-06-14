1445ef87398bffee17b3426fbd74d5a898ff6281
fede4ka <40442383+fede4ka@users.noreply.github.com>
2019-06-14 20:30:39 +0300

    Update TSP.scala

--- src/main/scala/TSP.scala
+++ src/main/scala/TSP.scala
@@ -2,7 +2,7 @@ object TSP extends App {
   //traveling salesman problem,brute force
   val testMap = Map (0 ->(1,1), 1->(1,2),2->(1,3),3->(2,3),4->(2,2),5->(2,1))
 
-  def distance(x:(Int,Int), y:(Int,Int)): Double =
+  def EuclideanDistance(x:(Int,Int), y:(Int,Int)): Double =
     math.sqrt((x._1 - y._1)*(x._1 - y._1) + (x._2 - y._2)*(x._2 - y._2))
 
   def routesPermutations(points:Range) = points.permutations.filter(x => x(1) < x.reverse.head)
@@ -10,7 +10,7 @@ object TSP extends App {
 
   def shortestRoot(map: Map[Int,(Int,Int)]): (IndexedSeq[(Int, Int)], Double) =  {
     lazy val distances = (for(i <- 0 until map.size ;
-                              j <- 0 until map.size) yield (i -> j) -> distance(map(i),map(j))).toMap
+                              j <- 0 until map.size) yield (i -> j) -> EuclideanDistance(map(i),map(j))).toMap
     lazy val routes = routesPermutations(0 until map.size).map(x => x zip (x.tail :+ x.head)).toVector
     routes zip routes.par.map(x => x.foldLeft(0.0)((s,v) => s + distances(v)))minBy(_._2)
   }
